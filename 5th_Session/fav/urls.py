c30e5a8f1efe75d82fe2ce0891291ab1c851f539
kimjieun <lucy7719@naver.com>
2020-05-24 21:46:00 +0900

    5,6_Sessions

--- 5th_Session/fav/urls.py
+++ 5th_Session/fav/urls.py
@@ -1,15 +1,17 @@
-from django.conf.urls import url
 from django.contrib import admin
+from django.urls import path
 from youtube import views
 from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
-    url('detail/<int:detail_id>',views.detail,name='detail'),
-    url(r'^admin/', admin.site.urls),
-    url('', views.home, name='home'),
+        path('admin/', admin.site.urls),
+    path('', views.home, name="home"),
+    path('create/',views.create,name="create"),
+    path('new/',views.new,name="new"),
+    path('detail/<int:detail_id>',views.detail,name='detail'),
 ]
 
 if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL,
-        document_root = settings.MEDIA_ROOT)
+    urlpatterns += static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
+
--- 5th_Session/youtube/views.py
+++ 5th_Session/youtube/views.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
-from django.shortcuts import render,get_object_or_404, redirect
+from django.shortcuts import render,get_object_or_404,redirect
 from .models import Video
 
 # Create your views here.
@@ -15,3 +15,17 @@ def home(request):
 
 def create(request):
     Youtube = Video()
+    Youtube.channel=request.POST['name']
+    Youtube.name=request.POST['creator']
+    Youtube.sub=request.POST['subscribe_num']
+    Youtube.linkone=request.POST['youtube_link_1']
+    Youtube.linktwo=request.POST['youtube_link_2']
+    Youtube.linkthree=request.POST['youtube_link_3']
+    Youtube.summary=request.POST['summary']
+    Youtube.text=request.POST['text']
+    Youtube.livecast=request.POST['choices']
+    Youtube.save()
+    return redirect('home')
+
+def new(request):
+    return render(request,'new.html')
