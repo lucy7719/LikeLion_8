4c7adb0d49ad1cd4d5166121d78bc7737780e474
kimjieun <lucy7719@naver.com>
2020-07-07 18:04:12 +0900

    pic error modify

--- summervacation_warmingup/pictureproject/firstapp/models.py
+++ summervacation_warmingup/pictureproject/firstapp/models.py
@@ -2,10 +2,6 @@ from django.db import models
 from django import forms
 from datetime import datetime
 
-class UploadFileForm(forms.Form):
-    titles = forms.CharField(max_length=100)
-    file = forms.FileField()
-
 class photo(models.Model):
     objects = models.Manager()
     name = models.CharField(max_length=100, default="작품 이름을 입력해 주세요")
